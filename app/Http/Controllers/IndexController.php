5f9a873c69f391409b6438f20076a2f26b8c7c69
KanekiKen124 <118106546+KanekiKen124@users.noreply.github.com>
2023-01-19 18:22:01 +0300

    Laravel Init

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $users = [
+            [
+                "id" => 1,
+                "username" => "Ilnur"
+            ],
+            [
+                "id" => 2,
+                "username" = > "Denis"
+            ],
+        ];
+        $user = [
+            "id" => 1,
+            "username" => "Альберт"
+        ];
+        return view('index', compact('user', 'users'));
+    }
+}
